c696da63ece4ddf0168f90d999721c38bf7c8ae6
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-06-07 09:19:50 +0600

    add cors and express middleware done

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('pencil perfectionist is running on ', port);
