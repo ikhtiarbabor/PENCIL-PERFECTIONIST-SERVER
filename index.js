9e42ee9a022df4921bd188ee0e55a8251265ddaf
MOHAMMAD IKHTIAR UDDIN <ikhtiarbabor@gmail.com>
2023-06-07 09:38:04 +0600

    create a user colleciton and post a user on database

--- index.js
+++ index.js
@@ -10,6 +10,39 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+// Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1yvmtut.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client
+      .db('pencilPerfectionistDB')
+      .collection('users');
+
+    /* userCollection start*/
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    /* userCollection end*/
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   console.log('pencil perfectionist is running on ', port);
 });
